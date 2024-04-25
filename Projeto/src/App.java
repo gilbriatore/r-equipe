fe23682b7fbd0e46b69c48f2bc8e185b4a104697
Professor Jason Sobreiro <geucimar.briatore@gmail.com>
2024-04-25 11:18:02 -0300

    Main dev 02

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -1,5 +1,9 @@
+import br.edu.up.Livro;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Alteração DEV 02");
+
+        Livro livro = new Livro();
     }
 }
