a9ba8c52dea550b6ebcbce172d3a56db49bfafe0
Professor Jason Sobreiro <geucimar@gmail.com>
2024-04-25 11:15:51 -0300

    Main dev 01

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -1,5 +1,10 @@
+import br.edu.up.Tela;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Alteração DEV 01");
+
+        Tela tela = new Tela();
+
     }
 }
